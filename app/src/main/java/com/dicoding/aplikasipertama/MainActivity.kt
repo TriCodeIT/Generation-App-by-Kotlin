963bf6a2db1375ff9a3a1fe3f37bdd1a5dec0073
Tricodeit <bayu165kusuma@gmail.com>
2021-04-14 22:52:33 +0700

    Generation App

---
+++ app/src/main/java/com/dicoding/aplikasipertama/MainActivity.kt
@@ -0,0 +1,32 @@
+package com.dicoding.aplikasipertama
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        //inisialisasi komponen
+        var edTglLahir = findViewById<EditText>(R.id.ed_tanggal_lahir)
+        var btnKlik = findViewById<Button>(R.id.btn_klik)
+        var tvHasil = findViewById<TextView>(R.id.tv_hasil)
+
+        btnKlik.setOnClickListener {
+            val input = edTglLahir.text.toString()
+            val hasil = when (input.toInt()){
+                in 1946..1964 -> "Baby boomers"
+                in 1965..1980 -> "X"
+                in 1981..1995 -> "Millenial"
+                in 1996..2010 -> "Z"
+                else -> "I Don't Know"
+            }
+            tvHasil.text = "Kamu Termasuk Generasi $hasil"
+
+        }
+    }
+}
